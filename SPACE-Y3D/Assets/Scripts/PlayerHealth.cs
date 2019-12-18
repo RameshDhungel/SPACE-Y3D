5d50db845d44b2b1fbf29777fa050bddb8c924e0
abimael <abibarre.82@gmail.com>
2019-12-17 21:27:15 -0500

    Player health script

---
+++ SPACE-Y3D/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int healthStart = 100;
+    public int currentHealth;
+    PlayerMovement playerMov;
+    
+    bool isDead;
+    bool damage;
+    void Start()
+    {
+        playerMov = GetComponent<PlayerMovement>();
+        currentHealth = healthStart; 
+    }
+
+    void Update()
+    {
+        //once damage take damage
+        if (damage)
+        {
+            TakeDamage();
+        }
+
+        damage = false;
+    }
+
+    public void TakeDamage()
+    {
+        //subtract the currentHelath by the healthStart
+        damage = true;
+        currentHealth -= healthStart;
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+   public void Death()
+    {
+        if (isDead)
+        {
+            GameObject Player = GameObject.Find("Player");
+            Destroy(Player);
+            Time.timeScale = 0;
+        }
+
+
+    }
+
+}
