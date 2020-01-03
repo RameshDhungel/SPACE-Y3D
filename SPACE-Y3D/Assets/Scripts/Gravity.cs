ab51a7cd3783806930a4bc90b1f0bbd1f1d1b553
abimael <abibarre.82@gmail.com>
2020-01-02 22:21:21 -0500

    Gravity applied to mag and objects

---
+++ SPACE-Y3D/Assets/Scripts/Gravity.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+
+    private GameObject Planet;
+    private GameObject bodyPlaceholder;
+
+    float gravity = 10000;
+    bool OnGround = false;
+
+
+    float distanceToGround;
+    Vector3 Groundnormal;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        bodyPlaceholder = this.gameObject;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        Planet = GameObject.FindGameObjectWithTag("Planet");
+    }
+    void Update()
+    {
+      //GroundControl
+
+    RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 10))
+        {
+
+            distanceToGround = hit.distance;
+            Groundnormal = hit.normal;
+
+            if (distanceToGround <= 0.2f)
+            {
+                OnGround = true;
+            }
+            else
+            {
+                OnGround = false;
+            }
+
+
+        }
+
+
+
+        //
+
+        Quaternion toRotation = Quaternion.FromToRotation(transform.up, Groundnormal) * transform.rotation;
+transform.rotation = toRotation;   
+    }
+    private void FixedUpdate()
+    {
+        //GRAVITY and ROTATION
+
+        Vector3 gravDirection = (transform.position - Planet.transform.position).normalized;
+
+        if (OnGround == false)
+        {
+            rb.AddForce(gravDirection* -gravity * Time.deltaTime);
+
+        }
+        
+    }
+
+}
